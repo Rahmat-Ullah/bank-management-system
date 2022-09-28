778b0ccf58e18b63e7d8a1de2d80ad6ec2289cdd
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-09-28 11:24:37 +0600

    login page made interactive using js

---
+++ js/login.js
@@ -0,0 +1,22 @@
+//step 1. add click event handler with the submit button
+document.getElementById('btn-submit').addEventListener('click', function(){
+    //step 2. get the email address inside the email input field
+    //always remember to use .value to get text from an input field
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    emailField.value = ('');
+    //step 3. get password
+    //3.a: set id on the html element
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(email, password);
+    passwordField.value = ('');
+
+    //step 4. varify email and password and check whether valid user or invalid user
+    if(email === 'rahmatbd1533@gmail.com' && password === 'secret'){
+        console.log('valid user');
+    }
+    else{
+        console.log('invalid user');
+    }
+})
